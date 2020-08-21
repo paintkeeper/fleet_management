6dfb5cad9c015e087a701885874e3f85efa1ced1
Andrei Alekseenko <engelier@gmx.de>
2020-08-21 07:55:17 +0200

    Added profile for security configuration

--- src/main/kotlin/backend/Configuration.kt
+++ src/main/kotlin/backend/Configuration.kt
@@ -14,22 +14,6 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  ******************************************************************************/
 
-/*******************************************************************************
- * Copyright (c) 2020.
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- ******************************************************************************/
-
 package backend
 
 import io.jsonwebtoken.Jwts
@@ -38,6 +22,7 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.jooq.JooqAutoConfiguration
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -64,6 +49,7 @@ import javax.servlet.http.Cookie
 class Configuration
 
 @EnableWebSecurity
+@Profile("security")
 class SecurityConfig(
     private val clientRegistrationRepository: ClientRegistrationRepository
 ) : WebSecurityConfigurerAdapter() {
